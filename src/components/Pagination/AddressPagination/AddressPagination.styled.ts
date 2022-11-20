15a42479b35caf6d40a85fe524aa9c980d6c6a9f
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-20 20:42:57 -0300

    adicionado bug de paginação inicial, adicionado paginação no endereço

--- src/components/Pagination/AddressPagination/AddressPagination.styled.ts
+++ src/components/Pagination/AddressPagination/AddressPagination.styled.ts
@@ -5,13 +5,29 @@ export const PaginationContainer = styled.div`
     height: 34px;
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-end;
+    padding-bottom: 5px;
     background-color: var(--primary-color);
     position: fixed;
-    gap: 5px;
+    gap: 20px;
     bottom: 0;
+    .chevron {
+        padding-bottom: 5px;
+        transition: all ease-in-out 50ms;
+
+        &:hover{
+            fill: var(--detail-color);
+        }
+    }
+
     a {
+        display: flex;
+        align-items: flex-end;
+        font-size: 1.065rem;
+        font-weight: 500;
         color: var(--background-color);
+        transition: all ease-in-out 50ms;
+
         &:hover {
             color: var(--detail-color);
         }
--- src/components/Pagination/PersonaPagination/PersonaPagination.styled.ts
+++ src/components/Pagination/PersonaPagination/PersonaPagination.styled.ts
@@ -9,17 +9,25 @@ export const PaginationContainer = styled.div`
     padding-bottom: 5px;
     background-color: var(--primary-color);
     position: fixed;
-    gap: 16px;
+    gap: 20px;
     bottom: 0;
     .chevron {
         padding-bottom: 5px;
+        transition: all ease-in-out 50ms;
+
+        &:hover{
+            fill: var(--detail-color);
+        }
     }
+
     a {
         display: flex;
         align-items: flex-end;
         font-size: 1.065rem;
         font-weight: 500;
         color: var(--background-color);
+        transition: all ease-in-out 50ms;
+
         &:hover {
             color: var(--detail-color);
         }
